e496778c9daea4176c4a70a455dff02343249e48
Leo Dion <leogdion@brightdigit.com>
2021-04-05 18:10:13 -0400

    fixing issue #7

--- Tests/OptionsTests/MappedEnumTests.swift
+++ Tests/OptionsTests/MappedEnumTests.swift
@@ -1,40 +1,43 @@
-@testable import Options
-import XCTest
+#if canImport(XCTest)
+  @testable import Options
+  import XCTest
 
-internal final class MappedEnumTests: XCTestCase {
-  private static let text = "\"a\""
-  internal func testDecoder() throws {
-    // swiftlint:disable:next force_unwrapping
-    let data = Self.text.data(using: .utf8)!
-    let decoder = JSONDecoder()
-    let actual: MappedEnum<MockEnum>
-    do {
-      actual = try decoder.decode(MappedEnum<MockEnum>.self, from: data)
-    } catch {
-      XCTAssertNil(error)
-      return
+  internal final class MappedEnumTests: XCTestCase {
+    private static let text = "\"a\""
+    internal func testDecoder() throws {
+      // swiftlint:disable:next force_unwrapping
+      let data = Self.text.data(using: .utf8)!
+      let decoder = JSONDecoder()
+      let actual: MappedEnum<MockEnum>
+      do {
+        actual = try decoder.decode(MappedEnum<MockEnum>.self, from: data)
+      } catch {
+        XCTAssertNil(error)
+        return
+      }
+      XCTAssertEqual(actual.value, .a)
     }
-    XCTAssertEqual(actual.value, .a)
+
+    internal func testEncoder() throws {
+      let encoder = JSONEncoder()
+      let describedEnum: MappedEnum<MockEnum> = .init(value: .a)
+      let data: Data
+      do {
+        data = try encoder.encode(describedEnum)
+      } catch {
+        XCTAssertNil(error)
+        return
+      }
+
+      let dataText = String(bytes: data, encoding: .utf8)
+
+      guard let text = dataText else {
+        XCTAssertNotNil(dataText)
+        return
+      }
+
+      XCTAssertEqual(text, Self.text)
+    }
   }
 
-  internal func testEncoder() throws {
-    let encoder = JSONEncoder()
-    let describedEnum: MappedEnum<MockEnum> = .init(value: .a)
-    let data: Data
-    do {
-      data = try encoder.encode(describedEnum)
-    } catch {
-      XCTAssertNil(error)
-      return
-    }
-
-    let dataText = String(bytes: data, encoding: .utf8)
-
-    guard let text = dataText else {
-      XCTAssertNotNil(dataText)
-      return
-    }
-
-    XCTAssertEqual(text, Self.text)
-  }
-}
+#endif
--- Tests/OptionsTests/MappedValueCollectionRepresentedTests.swift
+++ Tests/OptionsTests/MappedValueCollectionRepresentedTests.swift
@@ -1,48 +1,51 @@
-@testable import Options
-import XCTest
+#if canImport(XCTest)
+  @testable import Options
+  import XCTest
 
-internal final class MappedValueCollectionRepresentedTests: XCTestCase {
-  internal func testRawValue() {
-    try XCTAssertEqual(MockEnum.rawValue(basedOn: "a"), 0)
-    try XCTAssertEqual(MockEnum.rawValue(basedOn: "b"), 1)
-    try XCTAssertEqual(MockEnum.rawValue(basedOn: "c"), 2)
-    try XCTAssertEqual(MockEnum.rawValue(basedOn: "d"), 3)
-  }
-
-  internal func testString() {
-    try XCTAssertEqual(MockEnum.mappedValue(basedOn: 0), "a")
-    try XCTAssertEqual(MockEnum.mappedValue(basedOn: 1), "b")
-    try XCTAssertEqual(MockEnum.mappedValue(basedOn: 2), "c")
-    try XCTAssertEqual(MockEnum.mappedValue(basedOn: 3), "d")
-  }
-
-  internal func testRawValueFailure() {
-    let caughtError: MappedValueCollectionRepresentedError?
-    do {
-      _ = try MockEnum.rawValue(basedOn: "e")
-      caughtError = nil
-    } catch let error as MappedValueCollectionRepresentedError {
-      caughtError = error
-    } catch {
-      XCTAssertNil(error)
-      caughtError = nil
+  internal final class MappedValueCollectionRepresentedTests: XCTestCase {
+    internal func testRawValue() {
+      try XCTAssertEqual(MockEnum.rawValue(basedOn: "a"), 0)
+      try XCTAssertEqual(MockEnum.rawValue(basedOn: "b"), 1)
+      try XCTAssertEqual(MockEnum.rawValue(basedOn: "c"), 2)
+      try XCTAssertEqual(MockEnum.rawValue(basedOn: "d"), 3)
     }
 
-    XCTAssertEqual(caughtError, .valueNotFound)
-  }
-
-  internal func testStringFailure() {
-    let caughtError: MappedValueCollectionRepresentedError?
-    do {
-      _ = try MockEnum.mappedValue(basedOn: .max)
-      caughtError = nil
-    } catch let error as MappedValueCollectionRepresentedError {
-      caughtError = error
-    } catch {
-      XCTAssertNil(error)
-      caughtError = nil
+    internal func testString() {
+      try XCTAssertEqual(MockEnum.mappedValue(basedOn: 0), "a")
+      try XCTAssertEqual(MockEnum.mappedValue(basedOn: 1), "b")
+      try XCTAssertEqual(MockEnum.mappedValue(basedOn: 2), "c")
+      try XCTAssertEqual(MockEnum.mappedValue(basedOn: 3), "d")
     }
 
-    XCTAssertEqual(caughtError, .valueNotFound)
+    internal func testRawValueFailure() {
+      let caughtError: MappedValueCollectionRepresentedError?
+      do {
+        _ = try MockEnum.rawValue(basedOn: "e")
+        caughtError = nil
+      } catch let error as MappedValueCollectionRepresentedError {
+        caughtError = error
+      } catch {
+        XCTAssertNil(error)
+        caughtError = nil
+      }
+
+      XCTAssertEqual(caughtError, .valueNotFound)
+    }
+
+    internal func testStringFailure() {
+      let caughtError: MappedValueCollectionRepresentedError?
+      do {
+        _ = try MockEnum.mappedValue(basedOn: .max)
+        caughtError = nil
+      } catch let error as MappedValueCollectionRepresentedError {
+        caughtError = error
+      } catch {
+        XCTAssertNil(error)
+        caughtError = nil
+      }
+
+      XCTAssertEqual(caughtError, .valueNotFound)
+    }
   }
-}
+
+#endif
